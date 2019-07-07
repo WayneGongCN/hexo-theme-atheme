5ae5b7213a953d90df0d48cfbcfcdcd2bc251392
wayne <zowiegong@gmail.com>
2019-07-07 23:05:53 +0800

    feat(sw): add service worker use to cache files

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,18 @@
-pluginsConfig && pluginsConfig.scrollToTop && createScriptTag('/js/scrollToTop.js')
+window.onload = function () {
+  // scrollTopTop
+  pluginsConfig && pluginsConfig.scrollToTop && createScriptTag('/js/scrollToTop.js')
+
+  // service worker
+  'serviceWorker' in navigator && navigator.serviceWorker.register('/sw.js')
+}
+
 
 // Utils
+/**
+ * 节流函数
+ * @param {function} fn
+ * @param {number} timeout 
+ */
 function throttle(fn, timeout = 250) {
   const self = this
   let timer = null
@@ -15,6 +27,10 @@ function throttle(fn, timeout = 250) {
   }
 }
 
+
+/**
+ * 动态创建 script 标签
+ */
 function createScriptTag (url) {
   const scriptTag = document.createElement('script')
   scriptTag.src = url
