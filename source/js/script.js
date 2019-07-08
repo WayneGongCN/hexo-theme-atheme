243db431e2a007a469e9f3bc3e3529a640c8e279
wayne <zowiegong@gmail.com>
2019-07-08 23:41:42 +0800

    feat(comments): add comments

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,53 @@
 window.onload = function () {
-  // scrollTopTop
-  pluginsConfig && pluginsConfig.scrollToTop && createScriptTag('/js/scrollToTop.js')
+  if (pluginsConfig) {
+    const { scrollToTop, comments, googleAnalytics } = pluginsConfig
 
-  // service worker
-  'serviceWorker' in navigator && navigator.serviceWorker.register('/sw.js')
+    // scrollTopTop
+    scrollToTop && createScriptTag('/js/scrollToTop.js')
+
+    // service worker
+    'serviceWorker' in navigator && navigator.serviceWorker.register('/sw.js')
+
+    // comments
+    comments && comments.enable && enableComments(comments)
+
+    // googleAnalytics
+    googleAnalytics && enableGoogleAnalytics(googleAnalytics)
+  }
+}
+
+
+/**
+ * 启用评论
+ * @param {Object} config 
+ */
+function enableComments(config) {
+  const defaultConfig = {
+    'issue-term': "pathname",
+    theme: "github-light",
+    crossorigin: "anonymous",
+    async: true,
+    ...config
+  }
+  createScriptTag(`https://utteranc.es/client.js`, defaultConfig)
+}
+
+
+/**
+ * 启用 Google 分析
+ * @param {string} id 
+ */
+function enableGoogleAnalytics(id) {
+  const scriptTag = createScriptTag(`https://www.googletagmanager.com/gtag/js?id=${id}`, { async: true })
+
+  scriptTag.onload = function (e) {
+    window.gtag = function () {
+      dataLayer.push(arguments)
+    }
+    window.dataLayer = window.dataLayer || []
+    gtag("js", new Date)
+    gtag("config", id)
+  }
 }
 
 
@@ -31,8 +75,17 @@ function throttle(fn, timeout = 250) {
 /**
  * 动态创建 script 标签
  */
-function createScriptTag (url) {
+function createScriptTag(url, attribute) {
   const scriptTag = document.createElement('script')
   scriptTag.src = url
+
+  if (attribute) {
+    for (const key in attribute) {
+      const value = attribute[key]
+      scriptTag.setAttribute(key, value)
+    }
+  }
+
   document.body.appendChild(scriptTag)
+  return scriptTag
 }
