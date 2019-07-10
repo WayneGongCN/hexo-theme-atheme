cb9fe6519c67044b3cea41394389e400d507c242
zowiegong <zowiegong@gmail.com>
2019-07-10 20:07:40 +0800

    refactor(script): 拆分 js 文件

---
+++ source/js/ga.js
@@ -0,0 +1,13 @@
+(function (id) {
+  const scriptTag = createScriptTag(`https://www.googletagmanager.com/gtag/js?id=${id}`, { async: true })
+
+  scriptTag.onload = function (e) {
+    window.gtag = function () {
+      dataLayer.push(arguments)
+    }
+    window.dataLayer = window.dataLayer || []
+
+    gtag("js", new Date)
+    gtag("config", id)
+  }
+})(pluginsConfig.googleAnalytics)
--- source/js/script.js
+++ source/js/script.js
@@ -9,76 +9,10 @@ window.onload = function () {
     'serviceWorker' in navigator && navigator.serviceWorker.register('/sw.js')
 
     // comments
-    comments && comments.enable && enableComments(comments)
+    comments && comments.enable && createScriptTag('/js/comments.js', { async: true })
 
     // googleAnalytics
-    googleAnalytics && enableGoogleAnalytics(googleAnalytics)
-  }
-}
-
-
-/**
- * 启用评论
- * @param {Object} config 
- */
-function enableComments(config) {
-  const triggerEl = document.querySelector('.markdown-body p:last-child')
-  const observe = new IntersectionObserver(onEmitTrigger)
-
-  observe.observe(triggerEl)
-  addEventListener('message', onCommentsLoaded)
-
-  // 滚动到文章末尾触发
-  function onEmitTrigger (e) {
-    const ioe = e[0]
-    const isIntersecting = ioe.isIntersecting
-    console.log(isIntersecting)
-    if (isIntersecting) loadCommentScript()
-  }
-
-  function loadCommentScript () {
-    observe.disconnect()
-
-    const defaultConfig = {
-      'issue-term': "pathname",
-      theme: "github-light",
-      crossorigin: "anonymous",
-      async: true,
-      ...config
-    }
-    createScriptTag(`https://utteranc.es/client.js`, defaultConfig)
-  }
-
-  
-  /**
-   * 评论加载完成后 添加样式 移除监听
-   * @param {*} e 
-   */
-  function onCommentsLoaded (e) {
-    if (e.origin !== 'https://utteranc.es') return
-
-    const commentsContainer = document.querySelector('.utterances')
-    commentsContainer && (commentsContainer.style['border-top'] = '1px solid #eaecef')
-    
-    removeEventListener('message', onCommentsLoaded)
-  }
-}
-
-
-/**
- * 启用 Google 分析
- * @param {string} id 
- */
-function enableGoogleAnalytics(id) {
-  const scriptTag = createScriptTag(`https://www.googletagmanager.com/gtag/js?id=${id}`, { async: true })
-
-  scriptTag.onload = function (e) {
-    window.gtag = function () {
-      dataLayer.push(arguments)
-    }
-    window.dataLayer = window.dataLayer || []
-    gtag("js", new Date)
-    gtag("config", id)
+    googleAnalytics && createScriptTag('/js/ga.js', { async: true })
   }
 }
 
