bf48de99f41b6ff3c9af51694a258f3b41b548c4
waynegong <waynegong@tencent.com>
2022-03-29 22:34:24 +0800

    feat: add service worker

---
+++ source/sw.js
@@ -0,0 +1,68 @@
+console.log("[SW] Hello from service worker");
+
+
+const VERSION = "1";
+const CACHE_NAME = `cache-${VERSION}`;
+const PREFETCH_FILES = [
+  // CSS
+  "/lib/bootstrap-5.1.3/css/bootstrap.min.css",
+  "/css/style.css",
+
+  // PAGES
+  "/index.html",
+  "/tags/index.html",
+  "/about/index.html",
+  "/favicon/favicon.png",
+];
+
+
+self.addEventListener("install", (e) => {
+  console.log("[SW] install");
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(PREFETCH_FILES);
+    })
+  );
+});
+
+
+self.addEventListener("activate", (e) => {
+  console.log("[SW] activate");
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("[SW] Deleting out of date cache:", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      if (r) {
+        console.log("[SW] hit cache " + e.request.url);
+        return r;
+      }
+
+      console.log("[SW] request " + e.request.url);
+      return fetch(e.request).then((response) => {
+        if (e.request.method === "GET") {
+          return caches.open(CACHE_NAME).then((cache) => {
+            console.log("[SW] Caching new resource: " + e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        } else {
+          return response;
+        }
+      });
+    })
+  );
+});
